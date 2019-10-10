5c8d5862472473a0ce911e90f6c60caa36b6cdb9
sbengts1 <sbengt1@asu.edu>
2019-10-09 23:21:27 -0700

    Did formatting

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -19,72 +19,72 @@ package cse360assign2;
  */
 public class AddingMachine {
 
-	/**
-	 * The total amount that can be changed by {@link #add(int)},
-	 * {@link #subtract(int)}, and {@link #clear()}.   
-	 */
-	private int total;
-	
-	/**
-	 * A string that stores the history of all transactions performed on the 
-	 * total.
-	 */
-	private String transactions;
-	
-	/**
-	 * A constructor for {@link AddingMachine} that sets the value of the total
-	 * and the value of the transactions string to 0.
-	 */
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
-		transactions = "0";
-	}
-	
-	/**
-	 * Returns the current value of the total.
-	 * 
-	 * @return the current value of the total
-	 */
-	public int getTotal () {
-		return total;
-	}
-	
-	/**
-	 * Adds the supplied value to the total and updates the transactions string
-	 * to include the add operation.
-	 * 
-	 * @param value the value to be added to the total
-	 */
-	public void add (int value) {
-		total += value;
-		transactions += " + " + String.valueOf(value);
-	}
-	
-	/**
-	 * Subtracts the supplied value from the total and updates the transactions
-	 * string to include the subtract operation.
-	 * 
-	 * @param value the value that will be subtracted from the total
-	 */
-	public void subtract (int value) {
-		total -= value;
-		transactions += " - " + String.valueOf(value);
-	}
-	
-	/**
-	 * Returns the transactions string which details the history of 
-	 * transactions performed on the total.
-	 */
-	public String toString () {
-		return transactions;
-	}
+    /**
+     * The total amount that can be changed by {@link #add(int)},
+     * {@link #subtract(int)}, and {@link #clear()}.   
+     */
+    private int total;
+    
+    /**
+     * A string that stores the history of all transactions performed on the 
+     * total.
+     */
+    private String transactions;
+    
+    /**
+     * A constructor for {@link AddingMachine} that sets the value of the total
+     * and the value of the transactions string to 0.
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+        transactions = "0";
+    }
+    
+    /**
+     * Returns the current value of the total.
+     * 
+     * @return the current value of the total
+     */
+    public int getTotal () {
+        return total;
+    }
+    
+    /**
+     * Adds the supplied value to the total and updates the transactions string
+     * to include the add operation.
+     * 
+     * @param value the value to be added to the total
+     */
+    public void add (int value) {
+        total += value;
+        transactions += " + " + String.valueOf(value);
+    }
+    
+    /**
+     * Subtracts the supplied value from the total and updates the transactions
+     * string to include the subtract operation.
+     * 
+     * @param value the value that will be subtracted from the total
+     */
+    public void subtract (int value) {
+        total -= value;
+        transactions += " - " + String.valueOf(value);
+    }
+    
+    /**
+     * Returns the transactions string which details the history of 
+     * transactions performed on the total.
+     */
+    public String toString () {
+        return transactions;
+    }
 
-	/**
-	 * Resets the value of the total and the value of the transactions string
-	 * to zero.
-	 */
-	public void clear() {
-		total = 0;
-		transactions = "0";
-	}
+    /**
+     * Resets the value of the total and the value of the transactions string
+     * to zero.
+     */
+    public void clear() {
+        total = 0;
+        transactions = "0";
+    }
 }
--- src/cse360assign2/AddingMachineTest.java
+++ src/cse360assign2/AddingMachineTest.java
@@ -21,62 +21,62 @@ import org.junit.jupiter.api.Test;
  */
 class AddingMachineTest {
 
-	/*
-	 * Assert that the initial value of the total is zero.
-	 */
-	@Test
-	void testGetTotal() {
-		AddingMachine machine = new AddingMachine();
-		assertTrue(machine.getTotal() == 0);
-	}
+    /*
+     * Assert that the initial value of the total is zero.
+     */
+    @Test
+    void testGetTotal() {
+        AddingMachine machine = new AddingMachine();
+        assertTrue(machine.getTotal() == 0);
+    }
 
-	/*
-	 * Assert that adding a value to the total updates it accordingly.
-	 */
-	@Test
-	void testAdd() {
-		int value = 5;
-		AddingMachine machine = new AddingMachine();
-		machine.add(value);
-		assertTrue(machine.getTotal() == value);
-	}
+    /*
+     * Assert that adding a value to the total updates it accordingly.
+     */
+    @Test
+    void testAdd() {
+        int value = 5;
+        AddingMachine machine = new AddingMachine();
+        machine.add(value);
+        assertTrue(machine.getTotal() == value);
+    }
 
-	/*
-	 * Assert that subtracting a value from the total updates it accordingly.
-	 */
-	@Test
-	void testSubtract() {
-		int value = 3;
-		AddingMachine machine = new AddingMachine();
-		machine.subtract(value);
-		assertTrue(machine.getTotal() == -value);
-	}
+    /*
+     * Assert that subtracting a value from the total updates it accordingly.
+     */
+    @Test
+    void testSubtract() {
+        int value = 3;
+        AddingMachine machine = new AddingMachine();
+        machine.subtract(value);
+        assertTrue(machine.getTotal() == -value);
+    }
 
-	/*
-	 * Assert that performing transactions creates the expected transactions 
-	 * string.
-	 */
-	@Test
-	void testToString() {
-		AddingMachine machine = new AddingMachine();
-		machine.add(4);
-		machine.subtract(2);
-		assertTrue(machine.toString().equals("0 + 4 - 2"));
-	}
+    /*
+     * Assert that performing transactions creates the expected transactions 
+     * string.
+     */
+    @Test
+    void testToString() {
+        AddingMachine machine = new AddingMachine();
+        machine.add(4);
+        machine.subtract(2);
+        assertTrue(machine.toString().equals("0 + 4 - 2"));
+    }
 
-	/*
-	 * Assert that clearing the machine resets the total and the transactions
-	 * string to zero.
-	 */
-	@Test
-	void testClear() {
-		AddingMachine machine = new AddingMachine();
-		machine.add(3);
-		machine.add(6);
-		machine.subtract(3);
-		machine.subtract(1);
-		machine.clear();
-		assertTrue(machine.getTotal() == 0 && machine.toString().equals("0"));
-	}
+    /*
+     * Assert that clearing the machine resets the total and the transactions
+     * string to zero.
+     */
+    @Test
+    void testClear() {
+        AddingMachine machine = new AddingMachine();
+        machine.add(3);
+        machine.add(6);
+        machine.subtract(3);
+        machine.subtract(1);
+        machine.clear();
+        assertTrue(machine.getTotal() == 0 && machine.toString().equals("0"));
+    }
 
 }
